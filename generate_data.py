8295bc5463e462375a866a23f2ac73f1138b18c9
Matheus Lima <matheus@sementedigital.com.br>
2020-10-01 10:46:46 -0300

    add generate_data.py to project

--- generate_data.py
+++ generate_data.py
@@ -1,8 +1,5 @@
 import datetime
 
-
-# from decimal import Decimal
-
 # função para cálculo dos dígitos validadores do nosso número e do código de barras
 def modulo_onze(sequencia, nn=False, cb=False):
     # inverte a sequência recebida
@@ -65,6 +62,7 @@ def gerar_digito_verificador(sequencia):
 
     return str(digito_verificador)
 
+
 # TODO: utilização de dicionário de dados
 class BoletoSantander(object):
     codigo_banco = "033"  # identificação do banco
@@ -80,10 +78,10 @@ class BoletoSantander(object):
         self.fator_vencimento = self.gerar_fator_vencimento(data_vencimento, self.fator_data)
         self.nosso_numero = self.gerar_nosso_numero()
         self.codigo_de_barras, self.linha_digitavel = self.gerar_boleto(self.codigo_banco, self.codigo_moeda,
-                                                                         self.campos_fixos,
-                                                                         self.fator_vencimento, valor_nominal,
-                                                                         self.codigo_beneficiario,
-                                                                         self.nosso_numero, self.modalidade_carteira)
+                                                                        self.campos_fixos,
+                                                                        self.fator_vencimento, valor_nominal,
+                                                                        self.codigo_beneficiario,
+                                                                        self.nosso_numero, self.modalidade_carteira)
 
     # TODO: geração automática do campo "nosso número" para geração de boletos
     # gera o nosso número e seu código verificador
@@ -139,215 +137,11 @@ class BoletoSantander(object):
     def checar_dv_cb(cb):
         return cb[4]
 
-    def formatar_linha_digitavel(self, linha_digitavel):
-       return linha_digitavel[0:5] + "." + linha_digitavel[5:10] + " " + linha_digitavel[10:15] + "." + linha_digitavel[15:21] + " " + linha_digitavel[21:26] + "." + linha_digitavel[26:32] + " " + linha_digitavel[32] + " " + linha_digitavel[33:]
-
-def gerar_primeiro_campo(codigo_banco, codigo_moeda, campo_fixo, codigo_beneficiario):
-    primeiro_campo = codigo_banco + codigo_moeda + campo_fixo + codigo_beneficiario[0:4]
-
-    print("---PRIMEIRO CAMPO DE DADOS---")
-    print("código banco: {}\ncódigo moeda: {}\ncampo fixo: {}\nPSK: {}".format(codigo_banco, codigo_moeda, campo_fixo,
-                                                                               codigo_beneficiario[0:4]))
-    print("primeiro campo sem código verificador:", primeiro_campo)
-
-    codigo_verificador = gerar_digito_verificador(primeiro_campo)
-    print("dígito verificador do campo:", codigo_verificador)
-    ipte_parcial = primeiro_campo + codigo_verificador
-
-    print("--------------------------------------------------")
-    print("primeiro campo com código verificador:", ipte_parcial)
-    print("--------------------------------------------------")
-
-    return ipte_parcial
-
-
-def gerar_segundo_campo(codigo_beneficiario, nosso_numero):
-    segundo_campo = codigo_beneficiario[4:] + nosso_numero[0:7]
-    print("---SEGUNDO CAMPO DE DADOS---")
-    print("código do beneficiário: {}\nnosso número: {}".format(codigo_beneficiario[4:], nosso_numero[0:7]))
-    print("segundo campo sem código verificador:", segundo_campo)
-
-    codigo_verificador = gerar_digito_verificador(segundo_campo)
-    print("dígito verificador do campo:", codigo_verificador)
-    ipte_parcial = segundo_campo + codigo_verificador
-
-    print("--------------------------------------------------")
-    print("segundo campo com código verificador:", ipte_parcial)
-    print("--------------------------------------------------")
-
-    return ipte_parcial
-
-
-def gerar_terceiro_campo(nosso_numero, iof, modalidade_carteira):
-    terceiro_campo = nosso_numero[7:] + iof + modalidade_carteira
-    print("---TERCEIRO CAMPO DE DADOS---")
-    print("nosso número: {}\niof: {}\nmodalidade da carteira: {}".format(nosso_numero[7:], iof, modalidade_carteira))
-    print("terceiro campo sem código verificador:", terceiro_campo)
-
-    codigo_verificador = gerar_digito_verificador(terceiro_campo)
-    print("dígito verificador do campo:", codigo_verificador)
-    ipte_parcial = terceiro_campo + codigo_verificador
-
-    print("--------------------------------------------------")
-    print("terceiro campo com código verificador:", ipte_parcial)
-    print("--------------------------------------------------")
-
-    return ipte_parcial
-
-
-def gerar_quarto_campo(verificador):
-    print("---QUARTO CAMPO DE DADOS---")
-    print("verificador do código de barras: ", verificador)
-    print("quarto campo (verificador do código de barras):", verificador)
-    print("--------------------------------------------------")
-
-
-def gerar_quinto_campo(fator_vencimento, valor_nominal):
-    print("---QUINTO CAMPO DE DADOS---")
-    print("fator vencimento: {}\nvalor nominal: {}".format(fator_vencimento, valor_nominal))
-    print("quinto campo de dados:", fator_vencimento + str(valor_nominal).replace(".", "").zfill(10))
-
-
-def gerar_cb(codigo_banco, codigo_moeda, fator_data, data_vencimento, valor_nominal, codigo_beneficiario, nosso_numero,
-             modalidade_carteira, cnab_400=True):  # função geradora de código de barras (em número)
-    """
-    Posição Tamanho Picture     Conteúdo
-    01-03   3       9 (03)      Identificação do Banco = 033
-    04-04   1       9 (01)      Código da moeda = 9 (real)
-    05-05   1       9 (01)      DV do código de barras
-    06-09   4       9 (04)      Fator de vencimento
-    10-19   10      9 (08)V99   Valor nominal
-    20-20   1       9 (01)      Fixo “9”
-    21-27   7       9 (07)      Código do beneficiário padrão Santander
-    28-40   13      9 (13)      Nosso Número com DV
-    41-41   1       9 (01)      Fixo “0”
-    42-44   3       9 (03)      Tipo de Modalidade Carteira
-                                101-Cobrança Rápida COM Registro
-                                104-Cobrança Eletrônica COM Registro
-    """
-    layout_cb = ""
-    if cnab_400:
-        campos_fixos = ["9", "00000", "0"]
-        valor_nominal = str(valor_nominal).replace(".", "").zfill(10)
-        print("--------------------------------------------------")
-        layout_cb = codigo_banco + codigo_moeda + fator_vencimento_cb(fator_data, data_vencimento) + valor_nominal + \
-                    campos_fixos[0] + codigo_beneficiario + campos_fixos[1] + nosso_numero[5:] + campos_fixos[
-                        2] + modalidade_carteira
-    return layout_cb
-
-
-def fator_vencimento_cb(data_vencimento, fator_data_vencimento):
-    return str((data_vencimento - fator_data_vencimento).days)
-
-
-# PRENCHIMENTO DE CAMPOS
-
-# Posição 	Conteúdo
-# 1 a 3    Código do banco
-# 4        Código da Moeda - 9 para Real ou 8 - outras moedas
-# 5        Fixo "9"
-# 6 a 9    PSK - Código beneficiário (4 primeiros digitos)
-# 10 a 12  Restante do PSK (3 digitos)
-# 13 a 19  7 primeiros digitos do Nosso Número (N/N)
-# 20 a 25  Restante do Nosso numero (8 digitos) - total 13 (incluindo digito verificador)
-# 26 a 26  IOS
-# 27 a 29  Tipo Modalidade Carteira
-# 30 a 30  Dígito verificador do código de barras
-# 31 a 34  Fator de vencimento (quantidade de dias desde 07/10/1997 até a data de vencimento)
-# 35 a 44  Valor do título
-
-"""
-1º Campo: composto pelo código do banco, código da moeda, campo fixo "9", quatro
-primeiras posições do código do beneficiário padrão Santander (PSK) e dígito verificador
-deste campo.
-"""
-
-codigo_banco = "033"  # código do banco Santander
-codigo_moeda = "9"  # código para moeda local (Real)
-campo_fixo = "9"  # campo fixo que nunca muda na posição 5 do primeiro campo
-codigo_beneficiario = "0282033"  # apenas as 4 primeiras posições [0:3]
-primeiro_campo = gerar_primeiro_campo(codigo_banco, codigo_moeda, campo_fixo,
-                                      codigo_beneficiario)  # chama a função que o retorna dígito verificador
-
-"""
-2º Campo: composto pelas 3 posições restantes do código do beneficiário
-Santander (PSK), nosso número (N/N) com as 7 primeiras posições e dígito verificador
-deste campo.
-"""
-nosso_numero = "566612457800"
-nosso_numero += modulo_onze(nosso_numero, True)
-# chama a função que o retorna dígito verificador do primeiro campo
-segundo_campo = gerar_segundo_campo(codigo_beneficiario, nosso_numero)
-
-"""
-3º Campo: composto pelas 6 primeiras posições restante do N/N, 1 posição
-referente ao IOF, 3 posições referente ao Tipo de Modalidade da Carteira mais o
-dígito verificador deste campo.
-"""
-
-iof = "0"
-modalidade_carteira = "101"  # cobrança rápida com registro
-terceiro_campo = gerar_terceiro_campo(nosso_numero, iof, modalidade_carteira)
-
-"""
-4º campo: dígito verificador do código de barras (DAC)
-"""
-codigo_de_barras = "0339204600000273719028203356661245780020101"
-verificador = modulo_onze(codigo_de_barras, False, True)
-gerar_quarto_campo(verificador)
-
-"""
-5º campo: composto pelas 4 primeiras posições do fator vencimento (*) e as 10
-últimas com o valor nominal do documento, com indicação de zeros a esquerda e sem
-edição (sem ponto e vírgula).
-"""
-
-fator_data = datetime.date(1997, 10, 7)
-data_vencimento = datetime.date(2003, 5, 15)
-fator_vencimento = fator_vencimento_cb(data_vencimento, fator_data)
-valor_nominal = 273.71
-gerar_quinto_campo(fator_vencimento, valor_nominal)
-
-"""
-Nota 1: editar os três primeiros campos com um ponto (.), a ser inserido entre a 5ª e 6ª
-posições de cada campo.
-
-Nota 2: os dados da representação numérica não se apresentam na mesma ordem do
-código de barras, mas sim de acordo com a seqüência descrita acima.
-
-Nota 3: os dígitos verificadores referentes aos campos 1, 2, 3, não são
-representados no código de barras.
-"""
-
-# CODÍGO DE BARRAS (teste)
-
-teste = gerar_cb(codigo_banco, codigo_moeda, fator_data, data_vencimento, valor_nominal, codigo_beneficiario,
-                 nosso_numero,
-                 modalidade_carteira,
-                 True
-                 )
-
-codigo_de_barras = codigo_de_barras[0:4] + verificador + codigo_de_barras[4:]
-print("código de barras final: {}".format(codigo_de_barras))
-
-ipte_completo = (primeiro_campo + segundo_campo + terceiro_campo + verificador + fator_vencimento + str(
-    valor_nominal).replace(".", "").zfill(10)).replace(".", "")
-print("IPTE Completo: ",
-      primeiro_campo + " " + segundo_campo + " " + terceiro_campo + " " + verificador + " " + fator_vencimento + str(
-          valor_nominal).replace(".", "").zfill(10))
-
-data_teste = datetime.date(2003, 5, 15)
-valor_teste = 273.71
-boleto = BoletoSantander(data_teste, valor_teste)
-vencimento = boleto.fator_vencimento
-nossonumero = boleto.nosso_numero
-codigobarras = boleto.codigo_de_barras
-print("--------------------------------------------------")
-print("Objeto BoletoSantander:\ndata: {}\nvalor: {}\nfator vencimento: {}".format(boleto.data_vencimento, boleto.valor_nominal,
-                                                                         boleto.fator_vencimento))
-print("nosso número: ", nossonumero)
-print("código de barras: ", codigobarras)
-print("dígito validador do códigod de barras: ", boleto.checar_dv_cb(codigobarras))
-print("IPTE Completo: ", boleto.linha_digitavel)
-print("Linha digitável formatada: ", boleto.formatar_linha_digitavel(boleto.linha_digitavel))
-print("--------------------------------------------------")
+    @staticmethod
+    def formatar_linha_digitavel(linha_digitavel):
+        return linha_digitavel[0:5] + "." + linha_digitavel[5:10] + " " + linha_digitavel[
+                                                                          10:15] + "." + linha_digitavel[
+                                                                                         15:21] + " " + linha_digitavel[
+                                                                                                        21:26] + "." + linha_digitavel[
+                                                                                                                       26:32] + " " + \
+               linha_digitavel[32] + " " + linha_digitavel[33:]
